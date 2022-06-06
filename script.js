4feae97d091590e9483faca911d364bd91b6487b
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-06 14:22:12 +0300

    add style.css, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+document.getElementById("uyt").addEventListener("scroll", myFunction);
+function myFunction() {
+    alert ("Hello World!");
+  }
+ 
